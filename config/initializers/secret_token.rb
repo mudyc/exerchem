d9797782dfc732b29c0383b6e1d93d8f1f253780
Matti Katila <matti.katila@gmail.com>
2014-02-08 00:58:11 +0200

    rails new; rails generate controller course index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exerchem::Application.config.secret_key_base = 'ebf75b1e30b73c2fd11d924291259551bb8c200aed5897c975fa027cbf2331d05aeffaead6cb74620922a01a6866688870d26bc902f7b2e19c991589d609e87f'
